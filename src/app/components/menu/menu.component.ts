56edfa730da1b9ecfa0b7899ecca0626a74116a5
Jumphoto <jumanahphotography@gmail.com>
2023-04-30 13:19:54 +0200

    Called RestaurantHistory

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -4,6 +4,8 @@ import { CoverComponent } from '../cover/cover.component';
 import { Router } from '@angular/router';
 import { MenuItem, MenuItemTypeEnum } from 'src/app/modules/menu-item';
 import { BackendService } from 'src/app/services/backend.service';
+import { getRandomNumberBetween } from 'src/app/utils/common-functions';
+import { delay, lastValueFrom, of } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
@@ -13,9 +15,7 @@ import { BackendService } from 'src/app/services/backend.service';
   imports: [CommonModule, CoverComponent],
 })
 export class MenuComponent implements OnInit {
-  mainCourses: MenuItem[] = [];
-  desserts: MenuItem[] = [];
-  // fetchRestaurantHistory: any;
+  restaurantHistory: string | null = null;
   menuItems: { appetizers: MenuItem[]; mainCourses: MenuItem[]; desserts: MenuItem[] } = {
     appetizers: [],
     mainCourses: [],
@@ -23,23 +23,6 @@ export class MenuComponent implements OnInit {
   };
 
   constructor(private router: Router, private backendService: BackendService) {}
-
-  // ngOnInit() {
-  //   this.backendService
-  //     .fetchMenuItems()
-  //     .then((items) => {
-  //       // this.appetizers = items.filter((item) => item.type === 'APPETIZER');
-  //       this.mainCourses = items.filter((item) => item.type === 'MAIN_COURSE');
-  //       this.desserts = items.filter((item) => item.type === 'DESSERT');
-  //       console.log(items);
-  //       console.log(this.mainCourses);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  //   // this.fetchRestaurantHistory();
-  //   // console.log(this.fetchRestaurantHistory);
-  // }
 
   ngOnInit(): void {
     this.backendService.fetchMenuItems().then((menuItems: any[]) => {
@@ -49,7 +32,17 @@ export class MenuComponent implements OnInit {
 
       this.menuItems = { appetizers, mainCourses, desserts };
     });
+
+    this.fetchRestaurantHistory();
   }
+
+  fetchRestaurantHistory() {
+    const serverDelay = getRandomNumberBetween(1000, 2000);
+    lastValueFrom(of('Restaurant history data').pipe(delay(serverDelay)))
+      .then((data) => (this.restaurantHistory = data))
+      .catch((error) => console.log(error));
+  }
+
   closeMenu() {
     this.router.navigate(['/cover']);
   }
