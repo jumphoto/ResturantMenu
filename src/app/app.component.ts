0c03c9dabe3ed2eb3d1cc5b8323c07c28a529e9c
Jumphoto <jumanahphotography@gmail.com>
2023-04-30 22:38:05 +0200

    Added menu dialog/modal for selected item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,23 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ResponsiveSidenavComponent } from './components/responsive-sidenav/responsive-sidenav.component';
 import { CoverComponent } from './components/cover/cover.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MenuDialogComponent } from './components/menu-dialog/menu-dialog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterModule, ResponsiveSidenavComponent, CoverComponent],
+  imports: [
+    RouterModule,
+    ResponsiveSidenavComponent,
+    CoverComponent,
+    MatDialogModule,
+    CommonModule,
+    MenuDialogComponent,
+  ],
 })
 export class AppComponent {
   navItems: { label: string; routerLink: string[] }[] = [
---
+++ src/app/components/menu-dialog/menu-dialog.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-menu-item-dialog',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h2>{{ data.item.name }}</h2>
+    <p>{{ data.item.description }}</p>
+    <p>{{ data.item.price }}</p>
+    <ul>
+      <li *ngFor="let ingredient of data.metadata.ingredients">{{ ingredient }}</li>
+      <!-- <li *ngFor="let ingredient of data.metadata.imageUrl">
+        {{ imageUrl }}
+
+        <img [src]="ingredient.imageUrl" />
+      </li> -->
+    </ul>
+    <button
+      mat-button
+      (click)="close()"
+    >
+      Close
+    </button>
+  `,
+})
+export class MenuDialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialogRef<MenuDialogComponent>) {}
+
+  close(): void {
+    this.dialogRef.close();
+  }
+}
