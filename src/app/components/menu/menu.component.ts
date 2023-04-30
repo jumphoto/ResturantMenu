18fb41e55e65688bd33ed82c83d6949b67c897ae
Jumphoto <jumanahphotography@gmail.com>
2023-04-30 10:33:09 +0200

    Updated to use ENUM to filter & testing history

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { CoverComponent } from '../cover/cover.component';
 import { Router } from '@angular/router';
-import { MenuItem } from 'src/app/modules/menu-item';
+import { MenuItem, MenuItemTypeEnum } from 'src/app/modules/menu-item';
 import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
@@ -13,26 +13,43 @@ import { BackendService } from 'src/app/services/backend.service';
   imports: [CommonModule, CoverComponent],
 })
 export class MenuComponent implements OnInit {
-  appetizers: MenuItem[] = [];
   mainCourses: MenuItem[] = [];
   desserts: MenuItem[] = [];
+  // fetchRestaurantHistory: any;
+  menuItems: { appetizers: MenuItem[]; mainCourses: MenuItem[]; desserts: MenuItem[] } = {
+    appetizers: [],
+    mainCourses: [],
+    desserts: [],
+  };
 
   constructor(private router: Router, private backendService: BackendService) {}
 
-  ngOnInit() {
-    this.backendService
-      .fetchMenuItems()
-      .then((items) => {
-        this.appetizers = items.filter((item) => item.type === 'APPETIZER');
-        this.mainCourses = items.filter((item) => item.type === 'MAIN_COURSE');
-        this.desserts = items.filter((item) => item.type === 'DESSERT');
-        console.log(items);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+  // ngOnInit() {
+  //   this.backendService
+  //     .fetchMenuItems()
+  //     .then((items) => {
+  //       // this.appetizers = items.filter((item) => item.type === 'APPETIZER');
+  //       this.mainCourses = items.filter((item) => item.type === 'MAIN_COURSE');
+  //       this.desserts = items.filter((item) => item.type === 'DESSERT');
+  //       console.log(items);
+  //       console.log(this.mainCourses);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  //   // this.fetchRestaurantHistory();
+  //   // console.log(this.fetchRestaurantHistory);
+  // }
 
+  ngOnInit(): void {
+    this.backendService.fetchMenuItems().then((menuItems: any[]) => {
+      const appetizers = menuItems.filter((item) => item.type === MenuItemTypeEnum.APPETIZER);
+      const mainCourses = menuItems.filter((item) => item.type === MenuItemTypeEnum.MAIN_COURSE);
+      const desserts = menuItems.filter((item) => item.type === MenuItemTypeEnum.DESSERT);
+
+      this.menuItems = { appetizers, mainCourses, desserts };
+    });
+  }
   closeMenu() {
     this.router.navigate(['/cover']);
   }
